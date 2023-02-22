3a15e112907031edf9c5a399e417e8ba84d82412
Jamie Cha <chaeunhee98@gmail.com>
2023-02-21 21:51:59 -0600

    react-router

---
+++ client/src/app.js
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <span>Home</span>
+    }
+
+])
